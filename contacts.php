dd6172eeb2c5dba7343990b5f6df3bcfee7d1146
abacusdeskgitmain <abacusdeskgit@gmail.com>
2016-07-04 11:32:47 +0530

    Add files via upload

---
+++ contacts.php
@@ -0,0 +1,412 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="icon" href="img/fevicon.png" type="image/png"  >
+    <title>East Side Textiles</title>
+    <meta charset="utf-8">
+ 
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">
+    <meta name = "format-detection" content = "telephone=no" />
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/camera.css">
+    <link rel="stylesheet" href="css/carousel.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css"> 
+
+		<script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.1.1.js"></script>
+    <script src="js/superfish.js"></script>
+    <script src="js/jquery.mobilemenu.js"></script>
+    <script src="js/jquery.cookie.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/camera.js"></script>
+    <!--[if (gt IE 9)|!(IE)]><!-->
+      <script src="js/jquery.mobile.customized.min.js"></script>
+    <!--<![endif]-->
+    <script>
+        $(document).ready(function(){
+          jQuery('.camera_wrap').camera();
+        });
+    </script> 
+    <script src="js/jquery.carouFredSel-6.1.0.js"></script>
+    <script src="js/jquery.touchSwipe.min.js"></script>
+    <script language="javascript">
+      $(window).load(function(){
+      //  Responsive layout, resizing the items
+        $('#carousel-fred').carouFredSel({
+        auto: false,
+        responsive: true,
+        width: '100%',    
+        scroll: 1,
+        prev: '#prev-carousel',
+        next: '#next-carousel',
+        pagination: false,
+        mousewheel: true,
+        items: {
+        height: 'auto',
+        width: '270',
+        visible: {
+        min: 1,
+        max: 4
+        }
+        },
+        swipe: {
+          onMouse: true,
+          onTouch: true
+          }
+        });
+      });
+    </script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ 
+  ga('create', 'UA-51749194-1', 'eastsidetextiles.com');
+  ga('send', 'pageview');
+ 
+</script>
+    
+
+	<!--[if lt IE 8]>
+  		<div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/img/upgrade.jpg"border="0"alt=""/></a></div>  
+ 	<![endif]-->
+  <!--[if lt IE 9]>
+    <link rel="stylesheet" href="css/docs.css">
+    <link rel="stylesheet" href="css/ie.css">
+    <script src="assets/js/html5shiv.js"></script>
+  <![endif]-->
+  
+  
+   <style type="text/css">
+   .hide_email {    background: url("img/captcha_bg.png") no-repeat scroll 0 0 transparent;
+    height: 46px;
+    margin: -39px 0 8px 0;
+    position: relative;
+    width: 180px;
+    z-index: 2;}
+ .email {  color:#000; width:180px; height:auto; letter-spacing:20px;  text-align:center; padding:0 6px 0 16px; margin-top:18px;
+  position:relative; z-index:1; opacity:0.32;   }
+
+ .email  .c1,
+ .email  .c2,
+ .email  .c3,
+ .email  .c4,
+ .email  .c5,
+ .email  .c6{font-family: 'SourceSansProRegular'; font-size:23px; float:left; width:25px;} 
+  
+ .email  .c1{ color:#003300; padding-top:6px;}
+ .email  .c2{ color:#009; padding-top:15px;}
+ .email  .c3{ color:#600; padding-top:14px;}
+ .email  .c4{ color:#C33; padding-top:6px; }
+ .email  .c5{ color:#099;padding-top:5px;}
+ .email  .c6{ color:#F30; padding-top:15px;}
+ 
+ 
+ 
+ .btn_1 { width:80px !important; background:#26aad0 !important; border:none !important;  border-radius:5px; -moz-border-radius:5px; -webkit-border-radius:5px; color:#FFF !important; padding:5px 10px !important; margin-top:10px !important; }
+
+</style> 
+</head>
+
+<body>
+<?php @session_start();
+?>
+<!--==============================header=================================-->
+<div class="main">
+  <header>
+     <div class="container">
+           <div class="navbar navbar_ clearfix">
+              <div class="navbar-inner">      
+                    <div class="clearfix"> 
+                      <h1 class="brand"><a href="index.html"><img src="img/logo.png" alt=""></a><span>Industrial theme summitate Q.</span></h1> 
+                      <div class="nav-collapse nav-collapse_ collapse">
+                          <ul class="nav sf-menu clearfix">
+                            <li class="active"><a href="index.html">Home</a></li>
+                            <li class="sub-menu"><a href="about_us.html">About us</a>
+                              
+                                    
+                                     <li> <a href="products.html">Our products</a><ul>
+                                <li><a href="products.html#p1">Blankets</a></li>
+                                <li class="sub-menu_1"><a href="products.html#p2">Fabrics</a>
+                               
+                                  </li></ul>
+                                    
+                                   
+                                    <li> <a href="quality.html">Quality </a> </li>
+                                    <li> <a href="contacts.php"> Contacts </a> </li>
+
+
+
+
+                                  </ul>
+                              
+                      </div>
+                    </div>
+             </div>  
+           </div>
+    </div> 
+  </header>
+   
+  <div class="camera_wrap_2">                    
+        	<img src="img/banner_aboutus.jpg">
+         
+      </div>
+      <div class="banner-slider">
+      
+      </div>
+  </div>
+  
+  
+  
+  <div id="content">
+    <div class="container"> 
+        <div class="row">
+          <article class="span12">
+           <figure class="map">
+     
+     <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3007.3373619249132!2d-74.13099499999998!3d41.083475!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2e12e667bd1cb%3A0x6f814d82ffbdba14!2s1175+Richmond+Rd!5e0!3m2!1sen!2s!4v1402299052298,iwloc=A&amp;output=embed" ></iframe>     
+           </figure>
+          </article>
+          <div class="clearfix"></div>
+          <article class="span4">
+            <h3> <figure class="font15">Postal address</h3>
+            <div class="div-adress">
+              <address class="adress">
+                <strong class="font15">
+                  Address :
+                </strong>
+                <em>
+                  <span>
+                  1175, Richmond Road, MAHWAH, NJ - 07430
+
+                  </span>
+                </em>
+                <em>
+                  <i>Telephone: </i>
+                  <span>+1 973-234-3941</span>
+                  
+                </em>
+                <!--<strong class="font15">
+                  Address 2:
+                </strong>--><em>
+                <i>Email: </i>
+                <span>
+                 info@eastsidetextiles.com
+                </span>
+                </em>
+              </address>
+            </div>
+
+          </article>
+          
+          
+           <?php 
+if(isset($_POST['con'])){
+	
+	if($_POST['enquirytype']==''){ $ermsg .='Select Enquiry Type Missing \n';}
+      if($_POST['name']==''){ $ermsg .='Name Missing \n';}
+	  if($_POST['phone']==''){ $ermsg .='Phone Missing \n';}
+      if($_POST['email']==''){ $ermsg .='Email missing \n';}
+      if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) { $ermsg .='Enter a Valid Email \n';}
+	  if($_POST['message']==''){ $ermsg .='Message Missing \n';}
+	  
+	  if($_POST['captcha']!=$_POST['test1']){ $ermsg .='Enter Valid Captcha\n';}
+	  
+if($ermsg!='')
+{ 
+
+echo '<script type="text/javascript">alert("'.$ermsg.'");</script>';
+
+}		
+		
+	else{	
+	$sub='East Side Textiles Enquiry- '.$_POST['enquirytype'].'';
+	$comm='<table width="505" border="0" cellpadding="5" cellspacing="0" style="font-family:Tahoma, Geneva, sans-serif; font-size:12px;">
+                   <tr>
+                     <td width="110" > Name:</td>
+                     <td width="375">'.$_POST['name'].'</td>
+                   </tr>
+                   <tr>
+                     <td>&nbsp;</td>
+                     <td>&nbsp;</td>
+                   </tr>
+				   <tr>
+                     <td width="110" > Email:</td>
+                     <td width="375">'.$_POST['email'].'</td>
+                   </tr>
+				   <tr>
+                     <td>&nbsp;</td>
+                     <td>&nbsp;</td>
+                   </tr>
+				   <tr>
+                     <td width="110" > Phone:</td>
+                     <td width="375">'.$_POST['phone'].'</td>
+                   </tr>
+				   <tr>
+                     <td>&nbsp;</td>
+                     <td>&nbsp;</td>
+                   </tr>
+				   <tr>
+                     <td width="110" > Message:</td>
+                     <td width="375">'.$_POST['message'].'</td>
+                   </tr>
+				   		  
+             </table>';
+		  
+    $to='anujbansal8@gmail.com, manu.mayank@gmail.com';
+	
+$header  = "From: ".$_POST['name']." <".$_POST['email'].">"."\r\n"; 
+$header .= "Reply-To: ".$_POST['name']." <".$_POST['email'].">"."\r\n";
+$header .= "Return-Path: ".$_POST['name']." <".$_POST['email'].">"."\r\n";
+$header .= "X-Mailer: PHP/" . phpversion(). "\r\n";
+$header .= 'MIME-Version: 1.0' . "\r\n"; 
+$header .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+
+$send=mail($to,$sub,$comm,$header);
+echo '<script type="text/javascript">alert("Your Message has been Sent!");</script>';
+echo '<script language="javascript">window.location="http://eastsidetextiles.com/"</script>';
+	}
+	}?>
+    
+    
+          <article class="span8">
+            <h3><figure class="font15">Contact form</h3>
+            <form id="contact-form" name="contact" method="post">
+              <div class="success" style="display: none;"> Contact form submitted! <strong>We will be in touch soon.</strong> </div>
+              <fieldset>
+                  <div>
+                  <div class="form-div-0">
+                      <label class="name">
+                        <select name="enquirytype">
+                        <option value="">Select Enquiry Type</option>
+                        <option value="For Sale">For Sale</option>
+                        <option value="For Purchase">For Purchase</option>
+                        </select>
+                       </label>
+                    </div>
+                    <div class="form-div-1">
+                      <label class="name">
+                        <input type="text" name="name" value="<?php echo htmlspecialchars($_POST['name']); ?>" placeholder="Name">
+                       </label>
+                    </div>
+                    
+                    <div class="form-div-2">
+                      <label class="name">
+                        <input type="text" name="email" value="<?php echo htmlspecialchars($_POST['email']); ?>" placeholder="Email">
+                        </label>
+                    </div>
+                    
+                    <div class="form-div-3">
+                      <label class="phone notRequired">
+                        <input type="text" name="phone" value="<?php echo htmlspecialchars($_POST['phone']); ?>" placeholder="Phone">
+                        </label>
+                    </div>
+                  </div>
+                  <div>
+                    <label class="message">
+                      <textarea name="message" placeholder="Message"><?php echo htmlspecialchars($_POST['message']); ?></textarea>
+                     
+                    </label>
+                  </div>
+                  <?php 
+
+if(isset($_SESSION['captcha3']))
+{
+unset($_SESSION['captcha3']);
+}
+
+ 
+
+$num_chars=6;//number of characters for captcha image
+$characters=array_merge(range(0,9),range('A','Z'),range('a','z'));//creating combination of numbers & alphabets
+shuffle($characters);//shuffling the characters
+
+
+//getting the required random 5 characters
+$captcha_text="";
+for($i=0;$i<$num_chars;$i++)
+{
+$captcha_text.=$characters[rand(0,count($characters)-1)];
+}
+
+  $_SESSION['captcha3'] =$captcha_text;// assigning the text into session
+  $test=$_SESSION['captcha3'];
+  
+echo '<div class="email"><div class="c1">'.substr($_SESSION['captcha3'], 0, 1).'</div><div class="c2">'.substr($_SESSION['captcha3'], 1, 1).'</div><div class="c3">'.substr($_SESSION['captcha3'], 2, 1).'</div><div class="c4">'.substr($_SESSION['captcha3'], 3, 1).'</div><div class="c5">'.substr($_SESSION['captcha3'], 4, 1).'</div><div class="c6">'.substr($_SESSION['captcha3'], 5, 1).'</div></div><div class="hide_email"></div>';
+
+?>
+<label for='message'>Enter the code above here :</label>
+<input id="captcha" name="captcha" placeholder="Captcha" type="text" style="text-transform:none;"><br>
+<input type="hidden" name="test1" value="<?php echo $test;?>" />
+
+                
+                
+              </fieldset>
+              <input type="submit"  name="con" value="Submit" class="btn_1">
+            </form>
+
+          </article>
+        </div>
+    </div>
+
+
+    <footer>
+    <div class="container">
+           <div class="row">
+              <article class="span5">
+                <a href="index.html"><img src="img/logo_1.png" alt=""></a>
+              </article>
+              <article class="span6">
+                <ul class="list-footer first-list_footer">
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="about_us.html">About us</a></li>
+                  <li><a href="products.html">Our Products</a></li>
+                  <li><a href="quality.html">Quality</a></li>
+                  <li><a href="contacts.php">Contact us</a></li>
+                </ul>
+              </article>
+             
+              
+              <article class="span2 offset1">
+                <div class="div_soc">
+                  <span>Stay connected:</span>
+                  <ul class="list_soc">
+                    <li><a href="#" class="icon"></a></li>
+                    <li><a href="#" class="icon_1"></a></li>
+                    <li><a href="#" class="icon_2"></a></li>
+                    <li><a href="#" class="icon_3"></a></li>
+                  </ul>
+                </div>
+              </article>
+              <div class="clearfix"></div>
+             
+         </div>
+    </div>
+    <div class="footer-bottom">
+      <div class="container">
+        <div class="row">
+          <article class="span12">
+            <span>Copyright East Side Textilese © 2014</span><br><!-- {%FOOTER_LINK} -->
+          </article>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  </div>
+
+</div>
+
+
+
+
+
+
+</body>
+</html>
